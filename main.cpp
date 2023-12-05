6ff84bdbbe5dc35a51af6863abb67ae3210a71da
StanislawOlech <stanislaw.olech.of@interia.pl>
2023-12-05 09:53:14 +0100

    main2 deletion

--- main.cpp
+++ main.cpp
@@ -1,91 +1,118 @@
+#include <array>
 #include <iostream>
 #include <list>
-#include <tuple>
+#include <random>
 
-struct tram{
-    std::list<std::tuple<int, int>> path;
+
+struct Point2D
+{
+    int start;
+    int end;
 };
 
-int main(int argc, char const *argv[])
+using Tram = std::list<Point2D>;
+
+
+template <typename T, std::size_t DEPTH, std::size_t COL, std::size_t ROW>
+using Matrix3D = std::array<std::array<std::array<T, ROW>, COL>, DEPTH>;
+
+
+
+int main()
 {
-    int n = 4;
-    int m = 4;
-    int time = 10;
-    int tram_capacity = 50;
-    int passengers[n][m][time];
+    constexpr int n = 4;
+    constexpr int m = 4;
+    constexpr int time = 10;
+    constexpr int tram_capacity = 50;
+
+    // int passengers[n][m][time];
+    Matrix3D<int, n, m, time> passengers;
+
+    // Random numbers
+    std::random_device rd;
+    std::mt19937 generator{rd()};
+    std::uniform_int_distribution<> int_distribution{0, 50};
+
 
     // determinate numbers of passengers
-    for(int i=0; i < n; i++){
-        for(int j=0; j < m; j++){
-            for(int t=0; t < time; t++){
-                passengers[i][j][t] = rand() % 50;
-            } 
+    for (auto &coll: passengers)
+    {
+        for (auto &row: coll)
+        {
+            for (auto &x: row)
+                x = int_distribution(generator);
         }
     }
 
     // generate trams
-    std::list<tram> trams;
-    for(int i=0; i < n; i++){
-        tram tram_;
-        tram_.path = {std::make_tuple(i, 0), std::make_tuple(i, 1), std::make_tuple(i, 2), std::make_tuple(i, 3)};
-        trams.push_back(tram_);
+    std::list<Tram> trams;
+
+    for(int i = 0; i < n; ++i){
+        trams.emplace_back( Tram({{i, 0}, {i, 1}, {i, 2}, {i, 3}}) );
+
     }
-    for(int j=0; j < m; j++){
-        tram tram_;
-        tram_.path = {std::make_tuple(0, j), std::make_tuple(1, j), std::make_tuple(2, j), std::make_tuple(3, j)};
-        trams.push_back(tram_);
+
+    for(int j=0; j < m; ++j){
+        trams.emplace_back( Tram({{0, j}, {1, j}, {2, j}, {3, j}}) );
     }
 
 
-    // show passengers 
+    // show passengers
     for(int t=0; t < time; t++){
+
         for(int i=0; i < n; i++){
             std::cout << i << " ";
-            for(int j=0; j < m; j++){
+
+            for(int j=0; j < m; j++)
                 std::cout << passengers[i][j][t] << " ";
-            }
-            std::cout << std::endl;
+
+            std::cout << "\n";
         }
         std::cout << std::endl;
 
-        std::list<tram> new_trams;
-        for (auto tram_ : trams) {
+
+        std::list<Tram> new_trams;
+
+        for (auto tram_: trams) {
             // extract stop
-            auto stop = tram_.path.begin();
-            if (stop == tram_.path.end()) {
+            auto stop = tram_.begin();
+
+            if (stop == tram_.end()) {
                 continue;
             }
-            tram_.path.erase(stop);
-            int stop_i = std::get<0>(*stop);
-            int stop_j = std::get<1>(*stop);
+
+            tram_.erase(stop);
+            int stop_i = stop->start;
+            int stop_j = stop->end;
 
             // extract destination
-            auto dest = tram_.path.begin();
-            if (dest == tram_.path.end()) {
+            auto dest = tram_.begin();
+            if (dest == tram_.end()) {
                 continue;
             }
-            int dest_i = std::get<0>(*dest);
-            int dest_j = std::get<1>(*dest);
+
+            int dest_i = dest->start;
+            int dest_j = dest->end;
 
             // load passengers
-            passengers[stop_i][stop_j][t] = (passengers[stop_i][stop_j][t] > tram_capacity)? passengers[stop_i][stop_j][t] - tram_capacity: 0;
+            auto &curr_passengers = passengers[stop_i][stop_j][t];
+            curr_passengers = (curr_passengers > tram_capacity)? curr_passengers - tram_capacity: 0;
 
             new_trams.push_back(tram_);
         }
         trams = new_trams;
 
 
-
-
-        for(int i=0; i < n; i++){
+        for(int i = 0; i < n; ++i) {
             std::cout << i << " ";
-            for(int j=0; j < m; j++){
+
+            for(int j = 0; j < m; ++j) {
                 std::cout << passengers[i][j][t] << " ";
             }
-            std::cout << std::endl;
+            std::cout << "\n";
         }
         std::cout << std::endl;
-    } 
+    }
 
     return 0;
 }
