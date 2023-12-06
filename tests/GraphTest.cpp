72dfed9d8f936f14381de9ca4a2e93badbe2e618
StecBartlomiej <stecb@student.agh.edu.pl>
2023-12-06 21:58:51 +0100

    Added test, with basic usage

---
+++ tests/GraphTest.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "Graph.hpp"
+
+
+TEST(GrapTest, GeneralUse)
+{
+    std::vector<Point2D> vec1 = {{1, 2}, {3, 1}, {5, 6}};
+    std::vector<Point2D> vec2 = {{9, 0}, {9, 9}};
+
+    Graph<Point2D> graph{};
+
+    // using std::initialization_list
+//    graph.AddEdge({0, 0}, {{1, 2}, {3, 1}, {5, 6}});
+    graph.AddEdge({0, 0,}, vec1);
+
+    graph.AddEdge({0, 9}, vec2);
+
+    ASSERT_EQ(graph.GetEdge({0, 0}), vec1);
+    ASSERT_EQ(graph.GetEdge({0, 9}), vec2);
+}
